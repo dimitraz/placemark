b831f680a01c009260f1bb1fbd19a43f1900fa9c
dimitraz <dimitrazuccarelli@gmail.com>
2018-09-14 10:17:56 +0100

    :iphone: Initial commit

---
+++ app/src/main/java/com/example/mitch/placemark/PlacemarkActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mitch.placemark
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class PlacemarkActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_placemark)
+  }
+}
