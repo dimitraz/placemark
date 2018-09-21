5344b780653b3c7a22cb6c8f347b6bf003629a17
dimitraz <dimitrazuccarelli@gmail.com>
2018-09-21 10:47:57 +0100

    Exercise 2: add cancel button

--- app/src/main/java/com/example/mitch/placemark/activities/PlacemarkActivity.kt
+++ app/src/main/java/com/example/mitch/placemark/activities/PlacemarkActivity.kt
@@ -2,12 +2,16 @@ package com.example.mitch.placemark.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 import com.example.mitch.placemark.R
 import com.example.mitch.placemark.main.MainApp
 import com.example.mitch.placemark.models.PlacemarkModel
 import kotlinx.android.synthetic.main.activity_placemark.*
+import kotlinx.android.synthetic.main.activity_placemark_list.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
+import org.jetbrains.anko.startActivityForResult
 import org.jetbrains.anko.toast
 
 class PlacemarkActivity : AppCompatActivity(), AnkoLogger {
@@ -17,6 +21,8 @@ class PlacemarkActivity : AppCompatActivity(), AnkoLogger {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_placemark)
+    toolbarAdd.title = title
+    setSupportActionBar(toolbarAdd)
     app = application as MainApp
 
     btnAdd.setOnClickListener() {
@@ -36,5 +42,17 @@ class PlacemarkActivity : AppCompatActivity(), AnkoLogger {
         toast ("Please enter a valid title")
       }
     }
+  }
+
+  override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    menuInflater.inflate(R.menu.menu_placemark, menu)
+    return super.onCreateOptionsMenu(menu)
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+    when (item?.itemId) {
+      R.id.item_cancel -> finish()
+    }
+    return super.onOptionsItemSelected(item)
   }
 }
